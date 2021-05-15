9cce0f27214c9d136e610f205396f23dd9235104
Justin Michael Stringer <first-stringer@users.noreply.github.com>
2021-05-15 09:38:07 -0500

    add trivial change to test github hook

--- practice/algorithms/warmup/SolveMeFirst.java
+++ practice/algorithms/warmup/SolveMeFirst.java
@@ -1,26 +1,24 @@
 import java.io.*;
-import java.util.*;
-import java.text.*;
 import java.math.*;
+import java.text.*;
+import java.util.*;
 import java.util.regex.*;
 
 public class SolveMeFirst {
 
-// Added code is in this method.
-static int solveMeFirst(int a, int b) {
+  // Added code is in this method.
+  static int solveMeFirst(int a, int b) {
+                       return a + b;
+  }
 
-	return a+b;
-	}
-
-public static void main(String[] args) {
-  Scanner in = new Scanner(System.in);
-  int a;
-  a = in.nextInt();
-  int b;
-  b = in.nextInt();
-  int sum;
-  sum = solveMeFirst(a, b);
-  System.out.println(sum);
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int a;
+    a = in.nextInt();
+    int b;
+    b = in.nextInt();
+    int sum;
+    sum = solveMeFirst(a, b);
+    System.out.println(sum);
   }
 }
-
