0996a387e7fdbe9deb0578335afec202f7159363
Justin Michael Stringer <first-stringer@users.noreply.github.com>
2021-05-19 05:01:48 -0500

    formatted code and input and output

---
+++ practice/algorithms/sorting/insertion-sort-part-2/Solution.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+/* 0016-HR: INSERTION SORT – PART 2 */
+
+class Result {
+
+  static void swapPositions(List<Integer> arr, int p1, int p2) {
+    Integer temp = arr.get(p1);
+    arr.set(p1, arr.get(p2));
+    arr.set(p2, temp);
+  }
+
+  /*
+   * Complete the 'insertionSort2' function below.
+   *
+   * The function accepts following parameters:
+   *  1. INTEGER n
+   *  2. INTEGER_ARRAY arr
+   */
+
+  public static void insertionSort2(int n, List<Integer> arr) {
+    // Write your code here
+    for (int i = 1; i < arr.size(); i++) {
+      for (int j = i; j > 0 && arr.get(j - 1) > arr.get(j); j--) {
+        swapPositions(arr, j - 1, j);
+      }
+      for (Integer itp : arr) System.out.print(itp + " ");
+      System.out.println();
+    }
+  }
+}
+
+public class Solution {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+    String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+    List<Integer> arr = new ArrayList<>();
+
+    for (int i = 0; i < n; i++) {
+      int arrItem = Integer.parseInt(arrTemp[i]);
+      arr.add(arrItem);
+    }
+
+    Result.insertionSort2(n, arr);
+
+    bufferedReader.close();
+  }
+}
